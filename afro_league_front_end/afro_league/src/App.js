3cd9e54cad7e801c31405a82e11f53922708284c
Adesegun Adedeji <adesegunadedeji@gmail.com>
2019-10-12 11:46:09 -0400

    Refactored Code

---
+++ afro_league_front_end/afro_league/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import League from './components/League/League';
+
+function App() {
+  return (
+    <div className="App">
+      <League/>
+    </div>
+  );
+}
+
+export default App;
---
+++ afro_league_front_end/afro_league/src/components/League/League.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class League extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            player: []
+        }
+    }
+
+    componentDidMount(){
+        console.log("Component did Mount");
+        this.getPlayer();
+    }
+
+    getPlayer = async() =>{
+        const player= await fetch('http://localhost:3001/leagues')
+        const parsedResponse = await player.json();
+        console.log(parsedResponse)	     
+        this.setState({
+           player: parsedResponse
+        })
+    }
+
+
+    render () {
+        const player = this.state.player.map((playerProfile)=>{
+        return <div key = {playerProfile.id}>
+                    <p>{playerProfile.player}</p>   
+            </div>
+        })
+        return ( 
+            <div className = "playerRoster">
+                <h1>Afro League Player Roster</h1>
+                {player}
+            </div>
+        )
+    }
+  }
+  export default League
