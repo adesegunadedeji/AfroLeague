ab3dc19cd20dd2e965f0742782ae1fb1bd859e3d
Adesegun Adedeji <adesegunadedeji@gmail.com>
2019-11-13 23:30:26 -0500

    First afroleague heroku commit

---
+++ afro_league/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FaGlassWhiskey,FaHeart } from 'react-icons/fa'
+
+function FooterComponent (){
+        return (
+            <div className = "footerSection">
+             <footer className ="footer">
+                   Made with Lots of <FaGlassWhiskey/>  © Adesegun Adedeji 2019
+                   </footer>
+            </div>
+        )
+}
+
+export default FooterComponent 
